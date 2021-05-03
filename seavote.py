77deedd704fe345d22c701c145cf9738104cbc26
vastutsav <utsavbarman@gmail.com>
2021-05-04 00:47:18 +0530

    Create seavote.py

---
+++ seavote.py
@@ -0,0 +1,12 @@
+# cook your dish here
+I = input
+T = int(I())
+for i in range(T):
+    n = int(I())
+    b = list(map(int, I().split()))
+    s = sum(b)
+    if s < 100 or (s-100)/n > 1:
+        print("NO")
+    else:
+        print("YES")
+    
